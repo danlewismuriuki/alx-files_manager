49a6d2ba8df032401a87858a408de707e81daefa
lsd <danlewismuriuki2@gmail.com>
2024-09-27 22:11:55 +0300

    feat: Add RedisClient with async/await support using promisify
    - Created a RedisClient class to handle Redis operations.
    - Added methods: `isAlive`, `get`, `set`, and `del` for Redis key management.
    - Utilized `util.promisify` to convert callback-based Redis functions into Promise-based ones, allowing the use of async/await.
    - Exported an instance of RedisClient for use in the application.

--- utils/redis.js
+++ utils/redis.js
@@ -10,14 +10,30 @@ class RedisClient {
             console.error('Redis client not connected to the server:', error);
         });
 
-        // this.getAsync = promisify(this.client.get).bind(this.client)
-        // this.setAsync = promisify(this.client.set).bind(this.client)
-        // this.delAsync = promisify(this.client.del).bind(this.client)
-
-        await this.client.set()
-        await this.client.get()
-        await this.client.del()
+        this.getAsync = promisify(this.client.get).bind(this.client)
+        this.setAsync = promisify(this.client.set).bind(this.client)
+        this.delAsync = promisify(this.client.del).bind(this.client)
     }
 
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        const value = await this.getAsync(key);
+        return value;
+    }
+
+    async set(key, value, duration) {
+        await this.setAsync(key, value);
+        this.client.expire(key, duration);
+    }
+
+    async del(key) {
+        await this.delAsync(key);
+    }
 
 }
+
+const redisClient = new RedisClient();
+export default redisClient;
