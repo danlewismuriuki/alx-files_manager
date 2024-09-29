77627cea927df5cc2382b7c3963abe62487b8894
lsd <danlewismuriuki2@gmail.com>
2024-09-29 22:15:19 +0300

    fix: Correct user registration logic and improve error handling
    - Fixed destructuring issue in UsersController to properly extract email and password from the request body.
    - Added error handling for missing password during user registration.
    - Enhanced response messages for better clarity.

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,7 +4,7 @@ const crypto = require('node:crypto');
 class UserController {
 
     static async postNew(req, res) {
-        const { email: password } = req.body;
+        const { email, password } = req.body;
 
         if (!email) {
             return res.status(400).json({ error: 'Missing email' });
