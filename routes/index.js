869b92aa04119b219a9d60e8a8408c6c411655e2
lsd <danlewismuriuki2@gmail.com>
2024-09-29 23:07:05 +0300

    feat(routes): add new endpoints for connect, disconnect, and user profile
    - Added GET /connect endpoint to AuthController.getConnect
    - Added GET /disconnect endpoint to AuthController.getDisconnect
    - Added GET /users/me endpoint to UserController.getMe

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,20 @@ const express = require('express');
 
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
 router.post('/users', UsersController.postNew);
+
+// Auth
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisConnect);
+router.get('/users/me', AuthController.getMe);
+
+
 
 module.exports = router;
