dc9a53e727a21c8c038c52c9197ab42478ebfa12
lsd <danlewismuriuki2@gmail.com>
2024-09-29 22:41:26 +0300

    fix: correct indentation and formatting in UsersController and redis utility

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,38 +1,36 @@
-const dbClient = require('../utils/db');
 const crypto = require('node:crypto');
+const dbClient = require('../utils/db');
 
 class UserController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
 
-    static async postNew(req, res) {
-        const { email, password } = req.body;
-
-        if (!email) {
-            return res.status(400).json({ error: 'Missing email' });
-        }
-
-        //If the password is missing, return an error Missing password with a status code 400
-        if (!password) {
-            return res.status(400).json({ error: ' Missing password' });
-        }
-
-        const existingUser = await dbClient.usersCollection.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ error: 'Already exist' });
-        }
-
-        const hashedPassw = crypto.createHash('sha1').update(password).digest('hex');
-
-        const newUser = {
-            email, // shorthand
-            password: hashedPassw,
-        };
-
-        const result = await dbClient.usersCollection.insertOne(newUser);
-        newUser._id = result.insertedId;
-
-        return res.status(201).json(newUser);
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
     }
 
+    // If the password is missing, return an error Missing password with a status code 400
+    if (!password) {
+      return res.status(400).json({ error: ' Missing password' });
+    }
+
+    const existingUser = await dbClient.usersCollection.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassw = crypto.createHash('sha1').update(password).digest('hex');
+
+    const newUser = {
+      email, // shorthand
+      password: hashedPassw,
+    };
+
+    const result = await dbClient.usersCollection.insertOne(newUser);
+    newUser._id = result.insertedId;
+
+    return res.status(201).json(newUser);
+  }
 }
 
 module.exports = UserController;
