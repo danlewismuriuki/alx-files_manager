8ea7535b86692fca2fd5dabbc86f96d5947460bf
lsd <danlewismuriuki2@gmail.com>
2024-09-30 16:20:45 +0300

    feat: Implement user session management in Redis
    - Store user ID in Redis upon successful authentication.
    - Set session expiration time to 24 hours (86400 seconds).
    - Enhance session management for improved performance and scalability.

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -14,5 +14,36 @@ class AuthController {
 
         const base64Credentials = authHeader.split(' ')[1];
         const decodedCredentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+        const [email, password] = decodedCredentials.split(':');
+
+        if (!email || !password) {
+            return res.status(401).json({ error: "Unauthorized" });
+        }
+
+        try {
+            const user = await dbClient.usersCollection.findOne({ email });
+
+            if (!user) {
+                return res.status(401).json({ error: "Unauthorized" });
+            }
+
+            const hashedPassw = crypto.createHash('sha1').update(password).digest('hex');
+            if (hashedPassw !== user.password) {
+                return res.status(401).json({ error: 'Unauthorized' });
+            }
+
+            const token = uuidv4();
+
+            const redisKey = `auth_${token}`;
+            await redisClient.set(redisKey, user._id.toString(), 86400);
+
+            return res.status(200).json({ token });
+        } catch (error) {
+            console.error('Error during authentication:', error);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
     }
 };
+
+module.exports = AuthController;
