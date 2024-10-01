38405ae5f2cf7b13462f5910bf4b890b0d3e26f3
lsd <danlewismuriuki2@gmail.com>
2024-10-01 20:45:52 +0300

    style: fix indentation and spacing issues in controllers
    - Corrected indentation across AuthController.js and UsersController.js to adhere to ESLint rules.
    - Removed unnecessary blank lines and ensured consistent comment formatting.
    - Deleted unused file utils/env_loader.js.

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,70 +1,66 @@
-const dbClient = require('../utils/db');
-const redisClient = require('../utils/redis');
 const crypto = require('crypto');
 const { v4: uuidv4 } = require('uuid');
-
+const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
 
 class AuthController {
-    static async getConnect(req, res) {
-        //const authHeader = req.headers['authorization'];
-        const authHeader = req.headers['authorization'] || req.headers['Authorization'];
+  static async getConnect(req, res) {
+    // const authHeader = req.headers['authorization'];
+    const authHeader = req.headers.authorization || req.headers.Authorization;
 
-
-        if (!authHeader || !authHeader.startsWith('Basic ')) {
-            return res.status(401).json({ error: 'Unauthorized' })
-        }
-
-        const base64Credentials = authHeader.split(' ')[1];
-        const decodedCredentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
-        const [email, password] = decodedCredentials.split(':');
-
-        if (!email || !password) {
-            return res.status(401).json({ error: "Unauthorized" });
-        }
-
-        try {
-            const user = await dbClient.usersCollection.findOne({ email });
-
-            if (!user) {
-                return res.status(401).json({ error: "Unauthorized" });
-            }
-
-            const hashedPassw = crypto.createHash('sha1').update(password).digest('hex');
-            if (hashedPassw !== user.password) {
-                return res.status(401).json({ error: 'Unauthorized' });
-            }
-            const token = uuidv4();
-
-            const redisKey = `auth_${token}`;
-            await redisClient.set(redisKey, user._id.toString(), 86400);
-
-            return res.status(200).json({ token });
-        } catch (error) {
-            console.error('Error during authentication:', error);
-            return res.status(500).json({ error: 'Internal Server Error' });
-        }
+    if (!authHeader || !authHeader.startsWith('Basic ')) {
+      return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    static async getDisconnect(req, res) {
-        console.log('Headers received:', req.headers); // 
-        //const token = req.headers['x-token'];
-        const token = req.headers['x-token'] || req.headers['X-Token'];
+    const base64Credentials = authHeader.split(' ')[1];
+    const decodedCredentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [email, password] = decodedCredentials.split(':');
 
-        console.log("Disconnect token received:", token);
-        const userId = await redisClient.get(`auth_${token}`);
-        console.log("User ID retrieved from Redis:", userId);
+    if (!email || !password) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
 
-        if (!userId) {
-            return res.status(401).json({ error: 'Unauthorized' });
-        }
+    try {
+      const user = await dbClient.usersCollection.findOne({ email });
 
-        await redisClient.del(`auth_${token}`);
-        console.log("Token deleted from Redis");
+      if (!user) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
 
+      const hashedPassw = crypto.createHash('sha1').update(password).digest('hex');
+      if (hashedPassw !== user.password) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+      const token = uuidv4();
 
-        return res.status(204).send();
-    };
+      const redisKey = `auth_${token}`;
+      await redisClient.set(redisKey, user._id.toString(), 86400);
 
-};
+      return res.status(200).json({ token });
+    } catch (error) {
+      console.error('Error during authentication:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  static async getDisconnect(req, res) {
+    console.log('Headers received:', req.headers); //
+    // const token = req.headers['x-token'];
+    const token = req.headers['x-token'] || req.headers['X-Token'];
+
+    console.log('Disconnect token received:', token);
+    const userId = await redisClient.get(`auth_${token}`);
+    console.log('User ID retrieved from Redis:', userId);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    await redisClient.del(`auth_${token}`);
+    console.log('Token deleted from Redis');
+
+    return res.status(204).send();
+  }
+}
 
 module.exports = AuthController;
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,63 +1,63 @@
 const crypto = require('node:crypto');
+const { ObjectId } = require('mongodb');
 const dbClient = require('../utils/db');
 const redisClient = require('../utils/redis');
-const { ObjectId } = require('mongodb');
 
 class UsersController {
-    static async postNew(req, res) {
-        const { email, password } = req.body;
+  static async postNew(req, res) {
+    const { email, password } = req.body;
 
-        if (!email) {
-            return res.status(400).json({ error: 'Missing email' });
-        }
-
-        // If the password is missing, return an error Missing password with a status code 400
-        if (!password) {
-            return res.status(400).json({ error: 'Missing password' });
-        }
-
-        const existingUser = await dbClient.usersCollection.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ error: 'Already exist' });
-        }
-
-        const hashedPassw = crypto.createHash('sha1').update(password).digest('hex');
-
-        const newUser = {
-            email, // shorthand
-            password: hashedPassw,
-        };
-
-        const result = await dbClient.usersCollection.insertOne(newUser);
-        newUser._id = result.insertedId;
-
-        return res.status(201).json({ id: newUser._id, email: newUser.email });
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
     }
-    static async getMe(req, res) {
-        const token = req.header('X-Token');
-        //console.log("Received token:", token);
 
-        const userId = await redisClient.get(`auth_${token}`);
-        //console.log("Retrieved user ID from Redis:", userId);
-        if (!userId) {
-            return res.status(401).json({ error: 'Unauthorized' });
-        }
-
-        try {
-            //const user = await dbClient.usersCollection.findOne({ _id: userId });
-            const user = await dbClient.usersCollection.findOne({ _id: ObjectId(userId) });
-
-            //console.log("Attempting to find user with ID:", userId); // 3
-            if (!user) {
-                return res.status(401).json({ error: 'Unauthorized' });
-            }
-            return res.status(200).json({ id: user._id, email: user.email });
-
-        } catch (error) {
-            //console.error('Error retrieving user profile:', error);  // 4
-            return res.status(500).json({ error: 'Internal Server Error ' });
-        }
+    // If the password is missing, return an error Missing password with a status code 400
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
     }
+
+    const existingUser = await dbClient.usersCollection.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassw = crypto.createHash('sha1').update(password).digest('hex');
+
+    const newUser = {
+      email, // shorthand
+      password: hashedPassw,
+    };
+
+    const result = await dbClient.usersCollection.insertOne(newUser);
+    newUser._id = result.insertedId;
+
+    return res.status(201).json({ id: newUser._id, email: newUser.email });
+  }
+
+  static async getMe(req, res) {
+    const token = req.header('X-Token');
+    // console.log("Received token:", token);
+
+    const userId = await redisClient.get(`auth_${token}`);
+    // console.log("Retrieved user ID from Redis:", userId);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    try {
+      // const user = await dbClient.usersCollection.findOne({ _id: userId });
+      const user = await dbClient.usersCollection.findOne({ _id: ObjectId(userId) });
+
+      // console.log("Attempting to find user with ID:", userId); // 3
+      if (!user) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+      return res.status(200).json({ id: user._id, email: user.email });
+    } catch (error) {
+      // console.error('Error retrieving user profile:', error);  // 4
+      return res.status(500).json({ error: 'Internal Server Error ' });
+    }
+  }
 }
 
 module.exports = UsersController;
