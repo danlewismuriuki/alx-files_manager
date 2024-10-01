f03abef1d94db8625db846dbd62b53075fe30707
lsd <danlewismuriuki2@gmail.com>
2024-10-01 21:05:17 +0300

    feat: add POST /files endpoint for file uploads
    - Implemented FilesController with postUpload method
    - Updated routes/index.js to include the new endpoint

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const AppController = require('../controllers/AppController');
 const AuthController = require('../controllers/AuthController');
 const UsersController = require('../controllers/UsersController');
+const FilesController = require('../controllers/FilesController');
 
 const router = express.Router();
 
@@ -15,5 +16,10 @@ router.get('/users/me', UsersController.getMe);
 // Auth
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
+
+
+// Files
+// POST / files => FilesController.postUpload
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
